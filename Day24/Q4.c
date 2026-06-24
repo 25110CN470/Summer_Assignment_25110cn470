544da5c382f6161fd85461834c8d32ea4314406d
sheeba8097 <sheeba8097@gmail.com>
2026-06-24 22:32:51 +0530

    Completed Day24 assignment

---
+++ Day24/Q4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int i, j, flag;
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+    printf("After removing duplicates: ");
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        flag = 0;
+
+        for(j = 0; j < i; j++)
+        {
+            if(str[i] == str[j])
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0)
+            printf("%c", str[i]);
+    }
+
+    return 0;
+}
